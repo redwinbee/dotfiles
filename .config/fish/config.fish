aa055344073fd9d1195ee8bc1940bf613366ad70
Edwin Bermudes <hey@redwinbee.me>
2025-06-09 14:49:18 -0400

    Add yazi configuration to fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -23,6 +23,15 @@ function fish_prompt
 	set_color normal
 end
 
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+
 # Aliases
 alias ll="eza --long --group-directories-first"
 alias lla="eza --all --long --group-directories-first"
