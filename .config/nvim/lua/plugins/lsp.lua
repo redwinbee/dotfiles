db65d4f59dbd8669b9b7a87f49f9c24111e7cce2
Edwin Bermudes <hey@redwinbee.me>
2026-02-07 10:33:56 -0500

    update

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -4,6 +4,7 @@ return {
         vim.lsp.enable("clangd")
         vim.lsp.enable("rust_analyzer")
         vim.lsp.enable("lua_ls")
+        vim.lsp.enable("pylsp")
     end,
 
     vim.lsp.config("lua_ls", {
